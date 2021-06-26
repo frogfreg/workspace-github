1946448be893ce031a1305edc396f3505af4aac0
Guillermo Fernandez <bengallme@gmail.com>
2021-06-26 15:34:56 -0500

    Added apollo client and test query

--- notedly-client/pages/_app.js
+++ notedly-client/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://13.57.31.12:4000",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
